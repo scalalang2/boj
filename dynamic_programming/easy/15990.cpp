92d17d1648a09d0f9af1f5e28030381dce80da1e
scalalang2 <cailhuiris@gmail.com>
2023-07-30 12:35:07 +0900

    updated

---
+++ dynamic_programming/easy/15990.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using pi = pair<int, int>;
+using pll = pair<ll, ll>;
+
+const int INF = 1e9 + 5;
+const ll llINF = 1e18;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    return 0;
+}
---
+++ easy/16204.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using pi = pair<int, int>;
+using pll = pair<ll, ll>;
+
+const int INF = 1e9 + 5;
+const ll llINF = 1e18;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int n, m, k;
+    cin >> n >> m >> k;
+    cout << min(m, k) + min(n-m, n-k) << endl;
+
+    return 0;
+}
